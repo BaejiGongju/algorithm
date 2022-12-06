5ee902610d9891db7a1aebf13ef8fea892d90208
BaejiGongju <108972104+BaejiGongju@users.noreply.github.com>
2022-12-06 16:36:16 +0900

    Update 분수의 덧셈.js

--- level 0/분수의 덧셈.js
+++ level 0/분수의 덧셈.js
@@ -1,7 +1,27 @@
 function solution(denum1, num1, denum2, num2) {
-  const denum = denum2 * num1 + denum1 * num2;
-  const num = num1 * num2;
-  const getGCD = (a, b) => (b === 0 ? a : getGCD(b, a % b));
-  const gcd = getGCD(denum, num);
-  return [denum / gcd, num / gcd];
+    // 분모
+    const denum = denum2 * num1 + denum1 * num2;
+    // 분자
+    const num = num1 * num2;
+  
+    // 최대공약수
+    const getGCD = (a, b) => (b === 0 ? a : getGCD(b, a % b));
+    const gcd = getGCD(denum, num);
+  
+    return [denum / gcd, num / gcd];
 }
+
+
+
+// 최대공약수 구하기
+
+// function getGCD(a, b) {
+//     if (b === 0) {
+//         return a;
+//     } else {
+//         return getGCD(b, a % b);
+//     }
+// }
+
+// console.log(getGCD(4,2)) 
+// '2' 출력
